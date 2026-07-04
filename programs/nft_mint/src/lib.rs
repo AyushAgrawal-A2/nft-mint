b5c7ad563f29d84347a3b82d13d4b135182b4083
AyushAgrawal-A2 <ayushagl06@gmail.com>
2026-07-04 17:10:38 +0530

    anchor init

---
+++ programs/nft_mint/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("JCy7VmaXAS28emJPFNTr8qjvT7BrXXnAs26his91vnhX");
+
+#[program]
+pub mod nft_mint {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        crate::instructions::initialize::handle_initialize(ctx)
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        crate::instructions::increment::handle_increment(ctx)
+    }
+}
